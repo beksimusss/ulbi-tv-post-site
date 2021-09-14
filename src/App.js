4ba5fbfafb4315a96d881cab8ea5a033fc3cb9aa
unknown <Bek>
2021-09-14 17:55:32 +0300

    release v1

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from "react";
+import '../src/styles/App.css'
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
+import Error from "./pages/Error";
+import AppRouter from "./components/AppRouter";
+import {AuthContext} from "./context";
+
+
+function App() {
+    const [isAuth, setIsAuth] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            setIsAuth(true)
+        }
+        setIsLoading(false);
+    }, [])
+    return (
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth,
+            isLoading
+        }}>
+            <BrowserRouter>
+                <Navbar/>
+                <AppRouter/>
+            </BrowserRouter>
+        </AuthContext.Provider>
+    )
+}
+
+export default App;
---
+++ src/pages/PostIdPage.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {useFetching} from "../hooks/useFetching";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/Loader/Loader";
+
+const PostIdPage = () => {
+    const params = useParams()
+    const [post, setPost] = useState({})
+    const [comments, setComments] = useState([])
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(id)
+        setPost(response.data);
+    })
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id)
+        setComments(response.data);
+    })
+
+    useEffect(() => {
+        fetchPostById(params.id)
+        fetchComments(params.id)
+    }, [])
+
+    return (
+        <div>
+            <h1>This is post number {params.id} page!</h1>
+            {isLoading
+                ? <Loader/>
+                : <div>{post.id}. {post.title}</div>
+            }
+            <h3>Comments</h3>
+            {isComLoading
+                ? <Loader/>
+                : <div>
+                    {comments.map(comm =>
+                        <div key={comm.id} style={{margin: '15px'}}>
+                            <h4>{comm.email}</h4>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
+            }
+        </div>
+    );
+};
+
+export default PostIdPage;
